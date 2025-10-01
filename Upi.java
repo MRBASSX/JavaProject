57adb7c9757671d18ffeba7c28c12923862b4314
mrbassx <abassiddrisu9222@gmail.com>
2025-10-01 16:33:01 +0000

    Updated....

--- Upi.java
+++ Upi.java
@@ -1,29 +1,32 @@
-import java.util.Scanner;
 
 public class Upi {
 
 
     public static void main(String[] args) {
 
-        Scanner Abass = new Scanner(System.in);
+        int test;
+        test = 1000000000;
+        System.out.println(test);
 
-        System.out.println("What is your name");
-        String MyName = Abass.nextLine();
+        // Scanner Abass = new Scanner(System.in);
 
-        System.out.println("What is your age");
-        int Age = Abass.nextInt();
+        // System.out.println("What is your name");
+        // String MyName = Abass.nextLine();
 
-        System.out.println("What is your profession");
-        String profession = Abass.nextLine();
+        // System.out.println("What is your age");
+        // int Age = Abass.nextInt();
 
-        System.out.println("What is your interest");
-        String interest = Abass.nextLine();
+        // System.out.println("What is your profession");
+        // String profession = Abass.nextLine();
 
-        System.out.println("What is your hobbies");
-        String hobbies = Abass.nextLine();
+        // System.out.println("What is your interest");
+        // String interest = Abass.nextLine();
 
-        System.out.println("What is your country");
-        String country = Abass.nextLine();
+        // System.out.println("What is your hobbies");
+        // String hobbies = Abass.nextLine();
+
+        // System.out.println("What is your country");
+        // String country = Abass.nextLine();
 
 
 
@@ -31,8 +34,8 @@ public class Upi {
 
         
         
-        System.out.println("My name is "+ MyName +". I am "+ Age+" years old and I live in "+country+". I am a "+profession+" with a strong interest in "+interest+". I enjoy learning new things, especially in areas that challenge me to think creatively and solve problems. In my free time, I like "+hobbies+", which help me relax and express myself. My goal is to grow both personally and professionally, while contributing positively to the people and community around me.\n" + //
-                        "");
+        // System.out.println("My name is "+ MyName +". I am "+ Age+" years old and I live in "+country+". I am a "+profession+" with a strong interest in "+interest+". I enjoy learning new things, especially in areas that challenge me to think creatively and solve problems. In my free time, I like "+hobbies+", which help me relax and express myself. My goal is to grow both personally and professionally, while contributing positively to the people and community around me.\n" + //
+        //                 "");
         
     }
     
