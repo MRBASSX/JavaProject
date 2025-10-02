9dc954e133a57d4649992b82494dc7d25ede53d5
mrbassx <abassiddrisu9222@gmail.com>
2025-10-02 14:47:01 +0000

    updated...

---
+++ Salamatu.java
@@ -0,0 +1,60 @@
+
+
+
+public class Salamatu{
+ 
+   public static void main(String[] args) {
+   
+    // primitive datatypes;
+    // 1.integers
+    
+    int erica;
+    erica = 120;
+    System.out.println(erica);
+    
+    // 2.char
+
+    char agatha;
+    agatha = 'c';
+    System.out.println(agatha);
+    // 3.boolean
+
+    boolean full;
+    full = 67 <= 67;
+    System.out.println(full);
+
+    // 4.String
+
+    String asif;
+    asif = "3rtkh";
+    System.out.println(asif);
+
+
+
+
+   int num[][]  = {
+    {4,4,5,7,7},
+    {4,4,5,7,7}
+    };
+
+    boolean boat[][] = {{true, false}};
+
+   Boolean boo = (true);
+   char cha = 'A';
+   String name = "kofi";
+  float myFloat = 123f;
+  Double MyDouble = 23.56;
+  byte MyByte = -128;
+  Long MyL = 24L;
+  Short MyS = 234;
+
+   System.out.println(num[1][4]);
+   System.out.println(boo);
+   System.out.println(cha);
+   System.out.println(name);
+
+} 
+
+}
+    
+
