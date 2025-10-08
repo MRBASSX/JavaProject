3d135d06a785c25f61abef401b357f46ad553a29
mrbassx <abassiddrisu9222@gmail.com>
2025-10-08 16:24:47 +0000

    updated....

---
+++ Operators.java
@@ -0,0 +1,37 @@
+public class Operators {
+public static void main(String[] args){
+
+
+
+    // Arithmatic Operators
+    // %,-, +, /,*,
+// assignment Operators
+// =, -=,*=,/=,+=,%=
+
+// Tenary
+boolean livingroom = true;
+boolean diningroom =false;
+ boolean presence = livingroom && diningroom;  
+
+String message = presence ? "presence": "abscence";
+System.out.println(message);
+
+
+
+// decrement and increment
+// ++, --, +=,-=
+
+// logical Operators
+// Add &&, or ||, not!
+
+// relational Operators
+// ==,<,>,<=,>=, 
+
+
+
+
+
+}
+
+    
+}
