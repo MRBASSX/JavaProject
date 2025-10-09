41f8762a8b3c6f1bcc2bddb283abd06a00378d1b
mrbassx <abassiddrisu9222@gmail.com>
2025-10-09 20:55:56 +0000

    up

--- Operators.java
+++ Operators.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Operators {
 public static void main(String[] args){
 
@@ -5,10 +8,14 @@ public static void main(String[] args){
 
     // Arithmatic Operators
     // %,-, +, /,*,
+
 // assignment Operators
 // =, -=,*=,/=,+=,%=
 
 // Tenary
+
+
+Scanner haseena = new Scanner(System.in);
 boolean livingroom = true;
 boolean diningroom =false;
  boolean presence = livingroom && diningroom;  
@@ -16,16 +23,24 @@ boolean diningroom =false;
 String message = presence ? "presence": "abscence";
 System.out.println(message);
 
+System.out.println("Please enter a number ");
+int number = haseena.nextInt();
 
+boolean even = number % 2  ==0;
+String feedback = even? "even": "odd";
+System.out.println(feedback);
+
+boolean dgfryh = even && true;
+System.out.println(dgfryh );
 
 // decrement and increment
 // ++, --, +=,-=
 
 // logical Operators
-// Add &&, or ||, not!
+// Add  &, or ||, not!
 
 // relational Operators
-// ==,<,>,<=,>=, 
+// ==,<,>,<=,>=, !=
 
 
 
