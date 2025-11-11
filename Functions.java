822ff0d9848e56c6ba7855fa9b7acd398c19e223
mrbassx <Abassiddrisu9222@gmail.com>
2025-11-11 15:56:02 +0000

    updated....

--- Functions.java
+++ Functions.java
@@ -1,9 +1,17 @@
 
 public class Functions {
 
-    public static void main(Mary[] agrs) {
+    public static void main(String[] agrs) {
 
-        SquareSize(80);
+
+        int number = 4;
+
+        SquareSize(number);
+
+        RectangularSize(600,number);
+         RectangularSize(200,200);
+        trianglesize(950,2670);
+   
     }
 
     public static void Mary() {
@@ -15,5 +23,16 @@ public class Functions {
 
        System.out.println("The size of the Square is " + (4 * length));
     }
+
+    public static void RectangularSize(double length,double breath){
+
+        System.out.println(" The size of the Rectangle is  " + ((2 * length)  +(2 * breath )));
+    }
+
+    public static void trianglesize(float height,float base){
+        System.out.println("The size of the triangle is "+ (0.5) * (height * base));
+    }
+
+    // FriendZone
 
 }
