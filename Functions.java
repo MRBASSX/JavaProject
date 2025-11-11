d33adeb18a19fedd3bf22454cfc1cc8888714522
mrbassx <Abassiddrisu9222@gmail.com>
2025-11-11 13:59:29 +0000

    up

--- Functions.java
+++ Functions.java
@@ -3,10 +3,17 @@ public class Functions {
 
     public static void main(Mary[] agrs) {
 
+        SquareSize(80);
     }
 
-    public void Mary() {
+    public static void Mary() {
         System.out.println("Mary Nkrumah");
+    }
+
+
+    public static void SquareSize(float length){
+
+       System.out.println("The size of the Square is " + (4 * length));
     }
 
 }
